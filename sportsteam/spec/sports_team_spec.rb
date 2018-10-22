cda42455881077397a171f6ed08a7e5a73bb34e9
Steven Kay <stevendkay@gmail.com>
2018-10-22 17:00:07 +0100

    refactored team tests to use setup

--- sportsteam/spec/sports_team_spec.rb
+++ sportsteam/spec/sports_team_spec.rb
@@ -4,58 +4,52 @@ require_relative('../sports_team')
 
 class TestSportsTeam < MiniTest::Test
 
+  def setup
+    @team = SportsTeam.new("Team 1", ["Steven","Cat"], "Megabus")
+  end
+
   def test_make_sportsteam
-    team = SportsTeam.new("Team 1", ["Steven","Cat"], "Megabus")
-    assert_equal("Team 1", team.name)
+    assert_equal("Team 1", @team.name)
   end
 
   def test_get_team_name
-    team = SportsTeam.new("Team 1", ["Steven","Cat"], "Megabus")
-    assert_equal("Team 1", team.name)
+    assert_equal("Team 1", @team.name)
   end
 
   def test_get_players
-    team = SportsTeam.new("Team 1", ["Steven","Cat"], "Megabus")
-    assert_equal(["Steven","Cat"], team.players)
+    assert_equal(["Steven","Cat"], @team.players)
   end
 
   def test_get_coach
-    team = SportsTeam.new("Team 1", ["Steven","Cat"], "Megabus")
-    assert_equal("Megabus", team.coach)
+    assert_equal("Megabus", @team.coach)
   end
 
   def test_set_coach_name
-    team = SportsTeam.new("Team 1", ["Steven","Cat"], "Megabus")
-    team.coach = "National Express"
-    assert_equal("National Express", team.coach)
+    @team.coach = "National Express"
+    assert_equal("National Express", @team.coach)
   end
 
   def test_add_player_to_team
-    team = SportsTeam.new("Team 1", ["Steven","Cat"], "Megabus")
-    team.add_player("Sniffles")
-    assert_equal(["Steven", "Cat", "Sniffles"], team.players)
+    @team.add_player("Sniffles")
+    assert_equal(["Steven", "Cat", "Sniffles"], @team.players)
   end
 
   def test_if_someone_is_in_team
-    team = SportsTeam.new("Team 1", ["Steven","Cat"], "Megabus")
-    assert_equal(true, team.has_player_called("Cat"))
+    assert_equal(true, @team.has_player_called("Cat"))
   end
 
   def test_start_at_zero_points
-    team = SportsTeam.new("Team 1", ["Steven","Cat"], "Megabus")
-    assert_equal(0, team.points)
+    assert_equal(0, @team.points)
   end
 
   def test_win_increments_score
-    team = SportsTeam.new("Team 1", ["Steven","Cat"], "Megabus")
-    team.update_points("win")
-    assert_equal(1, team.points)
+    @team.update_points("win")
+    assert_equal(1, @team.points)
   end
 
   def test_lose_score_is_unchanged
-    team = SportsTeam.new("Team 1", ["Steven","Cat"], "Megabus")
-    team.update_points("lose")
-    assert_equal(0, team.points)
+    @team.update_points("lose")
+    assert_equal(0, @team.points)
   end
 
 end
