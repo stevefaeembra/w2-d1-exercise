78e09e8ba521962fe3147573183591a591d64194
Steven Kay <stevendkay@gmail.com>
2018-10-22 16:25:24 +0100

    refactor to use accessors

--- sportsteam/spec/sports_team_spec.rb
+++ sportsteam/spec/sports_team_spec.rb
@@ -6,27 +6,27 @@ class TestSportsTeam < MiniTest::Test
 
   def test_make_sportsteam
     team = SportsTeam.new("Team 1", ["Steven","Cat"], "Megabus")
-    assert_equal("Team 1", team.get_team_name)
+    assert_equal("Team 1", team.name)
   end
 
   def test_get_team_name
     team = SportsTeam.new("Team 1", ["Steven","Cat"], "Megabus")
-    assert_equal("Team 1", team.get_team_name)
+    assert_equal("Team 1", team.name)
   end
 
   def test_get_players
     team = SportsTeam.new("Team 1", ["Steven","Cat"], "Megabus")
-    assert_equal(["Steven","Cat"], team.get_players)
+    assert_equal(["Steven","Cat"], team.players)
   end
 
   def test_get_coach
     team = SportsTeam.new("Team 1", ["Steven","Cat"], "Megabus")
-    assert_equal("Megabus", team.get_coach)
+    assert_equal("Megabus", team.coach)
   end
 
   def test_set_coach_name
     team = SportsTeam.new("Team 1", ["Steven","Cat"], "Megabus")
-    team.set_coach_name("National Express")
-    assert_equal("National Express", team.get_coach)
+    team.coach = "National Express"
+    assert_equal("National Express", team.coach)
   end
 end
--- sportsteam/sports_team.rb
+++ sportsteam/sports_team.rb
@@ -1,24 +1,12 @@
 class SportsTeam
+
+  attr_reader :name, :players, :coach
+  attr_writer :coach
+  
   def initialize(name, players, coach)
     @name = name
     @players = players
     @coach = coach
-  end
-
-  def get_team_name
-    return @name
-  end
-
-  def get_players
-    return @players
-  end
-
-  def get_coach
-    @coach
-  end
-
-  def set_coach_name(new_coach_name)
-    @coach = new_coach_name
   end
 
 end
